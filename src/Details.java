358ebdd040287bc08f80e2767e66f4aa440e7eb5
Alan <alan_fcb10@hotmail.com>
2019-11-18 00:08:56 -0600

    Incomplete Solution IMplementation

---
+++ src/Details.java
@@ -0,0 +1,29 @@
+import java.util.*;
+public class Details  {
+
+	public static void main(String args[]){
+
+		String a = "a";
+		System.out.println(a.substring(1));
+	 //   ArrayList<String> listofcountries = new ArrayList<String>();
+	 //   listofcountries.add("BB");
+	 //   listofcountries.add("Cb");
+	 //   listofcountries.add("aA");
+	   
+
+	 //   /*Unsorted List*/
+	 //   System.out.println("Before Sorting:");
+	 //   for(String counter: listofcountries){
+		// 	System.out.println(counter);
+		// }
+
+	 //   /* Sort statement*/
+	 //   Collections.sort(listofcountries);
+
+	 //   /* Sorted List*/
+	 //   System.out.println("After Sorting:");
+	 //   for(String counter: listofcountries){
+		// 	System.out.println(counter);
+		// }
+	}
+}
--- src/Main.java
+++ src/Main.java
@@ -13,22 +13,27 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Scanner;
 import java.util.Stack;
+import java.util.*;
 import java.io.*;
 
 public class Main {
+
     //Here we create a Hashmap that will store all the transitions of the grammar
     //the key of the Hashmap will be the non-terminal symbols and the content of it will be
     //a list of string with all the productions of the non terminal symbols
     private static HashMap<Character, ArrayList<String>> transitions = new HashMap<>();
+    //Here we store the final result (true/false)
+    private static boolean solution = false;
 
     public static void main(String[] args) throws IOException{
+        System.out.println("papa");
         File file; //this is the variable of the file that we will read
         Scanner scan; //this is the variable of the scanner that we need to read the file.txt
 
         //We need to add a try and catch function to read the file in case we don't find it
         try{
             //We initialize the file variable with the path of the file
-            file = new File("C:/Users/Navarro D. Gerardo/Documents/Quinto Semestre/mate computacional/Part2/src/test2.txt");
+            file = new File("test3.txt");
             //we need to add to the scanner the file so we can read it
             scan = new Scanner(file);
         }catch(FileNotFoundException e1){
@@ -52,11 +57,18 @@ public class Main {
             String t = scan.next(); //this String will have the productions of the grammar
             Character key = t.charAt(0); // This cahr will be the key of the Hashmap
             String content = t.substring(3); //this string will be contain the production of the non-terminal symbol
-
+            System.out.println("content: "+ content);
             if(!transitions.containsKey(key)){ //if the key is not in the hashmap it means we need to add it to the hashmap
                 transitions.put(key, new ArrayList<String>());//we initialize a new arraylist to the new key
             }
-            transitions.get(key).add(content);//we add the production to the arraylist of the key
+            if(content.equals("lmd")){
+                transitions.get(key).add("{");
+                Collections.sort(transitions.get(key));
+            }
+            else {
+                transitions.get(key).add(content);//we add the production to the arraylist of the key
+                Collections.sort(transitions.get(key));
+            }
         }
 
         //after we have read all the .txt we will do block of code
@@ -66,26 +78,41 @@ public class Main {
         Stack<Character> PDA = new Stack<>();
         //we add to the stack the initial non-terminal symbol
         PDA.add(start);
-        String testString = "ba";//tenemos que cambiar esta linea por un scanner para que usuario lo ponga
+        String testString = "b";//tenemos que cambiar esta linea por un scanner para que usuario lo ponga
         //we will print the final result of the method that will check if the string is produced by the grammar
-        System.out.println(checkString(PDA, testString));
+        checkString(PDA, testString);
+        System.out.println(solution);
     }
 
     //this function will check if the string is produced by the grammar
     private static boolean checkString(Stack<Character> PDA, String testString){
         System.out.println(PDA + " -> " + testString);
-        //if the stack of the production of the grammar is bigger than the string that we need to check
-        //it means this is not the path that we need to follow to reach to the string
-        if(PDA.size() > testString.length()) return false;
+
+        if(solution) return true;
+        
         //if the string is empty or the stack of production is empty we it means this si not the correct path
         if(testString.equals("") && !PDA.empty()) return false;
         if(!testString.equals("") && PDA.empty()) return false;
         //in case we have a empty production Stack and a empty string it means we have reach the string that we want
         //and we need to return true
-        if(PDA.empty() && testString.equals("")) return true;
+        if(PDA.empty() && testString.equals("")) {
+            solution = true;
+            return true;
+        }
 
         //first we need to pop a char from the production stack
         char c = PDA.pop();
+        System.out.println("char to "+ c);
+        if(c == '{'){
+            System.out.println("Alan");
+            c = PDA.pop();
+            System.out.println("Encontramos esto: "+c);
+            if(PDA.empty() && testString.equals("")){
+                System.out.println("Paola");
+                solution = true;
+                return true;
+            }
+        }
         //if the char is in the Hashmap it means that the char is a non-terminal symbol so we have productions to follow
         if(transitions.containsKey(c)){
             //we initialize a StringBuilder because we need to append the productions of the non-terminal symbols
@@ -95,7 +122,7 @@ public class Main {
                 sb.append(PDA.pop());
             }
             //we need to add a for so we can send all the different productions of the key
-            for(int i = 0; i < transitions.get(c).size(); i++){
+            for(int i = transitions.get(c).size() - 1; i >= 0; i--){
                 //we create a new stack that will be the new production that we will send to the funcion
                 Stack<Character> auxStack = new Stack<>();
 
@@ -119,6 +146,7 @@ public class Main {
             //in case the char that we pop isn't in the Hashmap it means that is a terminal symbol
             //if the char is the same as the char at the position 0 in the string that we want to validate
             //this means is a possible path to reach this string
+            
             if(c == testString.charAt(0)){
                 //we eliminate the char at the position 0
                 testString = testString.substring(1);
